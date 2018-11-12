c37e82857c6a9e899e02c4de7ebad5a12f9f7975
TySabs <tysabs@gmail.com>
2018-11-11 23:52:24 -0600

    Add Puzzle class

---
+++ Sudoku/Puzzle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sudoku
+{
+    enum PuzzleDifficulty { easy = 1, medium = 2, hard = 3 }
+
+    class Puzzle
+    {
+        bool IsSolved { get; set; }
+        bool IsCompleted { get; set; }
+        bool IsSaved { get; set; }
+        PuzzleDifficulty DifficultyLevel;
+        int Timer { get; set; }
+
+        // TODO add Initial State, Saved State, and Solution State
+    }
+}
